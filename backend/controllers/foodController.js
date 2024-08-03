470fc863858f879807122c55475b8bafb08f4772
Mig <goujonmael@gmail.com>
2024-08-03 15:21:37 +0200

    add, display, remove food items from the admin web page

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -39,7 +39,7 @@ const removeFood = async(req,res) => {
         const food = await foodModel.findById(req.body.id);
         fs.unlink(`uploads/${food.image}`,()=>{});
         await foodModel.findByIdAndDelete(req.body.id)
-        res.json({sucess:true,message:"Food Removed"})
+        res.json({success:true,message:"Food Removed"})
     } catch (error) {
         console.log(error)
         res.json({success:false,message:"Error"})
